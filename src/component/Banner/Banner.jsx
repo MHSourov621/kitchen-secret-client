f9a0729aca49cf650f1f821fd15a1b9f5a76e9c2
MHSourov621 <mhsourov621@gmail.com>
2023-05-03 17:34:48 +0600

    added another section

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import "./Banner.css"
 
 const Banner = () => {
     return (
-        <div className='banner-container text-white'>
+        <div className='banner-container text-white mb-48'>
             <div className='lg:w-2/5 p-10 lg:py-28 lg:pl-16'>
                 <h1 className='text-6xl mb-10'>Lets Start Cooking With Popular Recipes.</h1>
                 <p className='text-xl mb-4'>Want to learn cook but confuse how to start?</p>
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -5,6 +5,27 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <h2 className='text-4xl font-bold mb-14 text-center'>Recipes By Category</h2>
+            <div className='grid lg:grid-cols-3 lg:ml-16 mb-48'>
+                <div className="card w-96 bg-base-100 shadow-xl cursor-pointer">
+                    <figure><img src="https://leitesculinaria.com/wp-content/uploads/2021/10/classic-vanilla-cupcakes-1.jpg" alt="" /></figure>
+                    <div className="card-body items-center bg-black text-white">
+                        <h2 className="text-2xl ">Sweets</h2>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl cursor-pointer">
+                    <figure><img src="https://img.freepik.com/free-photo/double-hamburger-isolated-white-background-fresh-burger-fast-food-with-beef-cream-cheese_90220-1192.jpg?w=826&t=st=1683112455~exp=1683113055~hmac=43330c9ed6db8eb303da2d39d709de96023b3a6a73cc06a1f1effc48ca6b2f1c" alt="" /></figure>
+                    <div className="card-body items-center bg-black text-white">
+                        <h2 className="text-2xl ">Burger</h2>
+                    </div>
+                </div>
+                <div className="card w-96 bg-base-100 shadow-xl cursor-pointer">
+                    <figure><img src="https://img.freepik.com/free-photo/side-view-pizza-with-slices-bell-pepper-pizza-slices-flour-board-cookware_176474-3185.jpg?w=826&t=st=1683112948~exp=1683113548~hmac=35785796cc4f630feb7ef0a608a84734a570dea789c05da3546a51eec7dcf22c" alt="" /></figure>
+                    <div className="card-body items-center bg-black text-white">
+                        <h2 className="text-2xl ">Pizza</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
