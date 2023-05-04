ea49b92d7523e46c08523d77f3bf717190be0bce
MHSourov621 <mhsourov621@gmail.com>
2023-05-04 18:36:27 +0600

    uncomment error

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
-    // errorElement: <ErrorPage></ErrorPage> ,
+    errorElement: <ErrorPage></ErrorPage> ,
     children: [
       {
         path: "/",
